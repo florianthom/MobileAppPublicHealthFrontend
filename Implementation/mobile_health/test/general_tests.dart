7b9420e5b4ec5045fdc645264d1bfb3e4920fac7
Rico Stucke <rico_stucke@efa79.de>
2021-01-26 22:03:47 +0100

    added basic unit tests for StatisticsAnalyzer

--- Implementation/mobile_health/test/general_tests.dart
+++ Implementation/mobile_health/test/general_tests.dart
@@ -4,12 +4,12 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mobile_health/bloc/navbar/navbar_bloc.dart';
 import 'package:mobile_health/bloc/table_calender/table_calender_bloc.dart';
-import 'package:mobile_health/components/HomeCategoryHeader.dart';
 import 'package:mobile_health/components/MoreOptionBox.dart';
-import 'package:mobile_health/main.dart';
 import 'package:mobile_health/screens/HomePageScreen.dart';
 import 'package:mobile_health/screens/MoreScreen.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart'; // Add this line
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:mobile_health/statistics/Statistics.dart';
+import 'package:scidart/numdart.dart';
 
 ///
 ///
@@ -117,14 +117,52 @@ void main() {
       ));
     }
 
-    Future<Null> _screenRendersProgressIndicatorWhileLoading(WidgetTester tester) async {
+    Future<Null> _screenRendersProgressIndicatorWhileLoading(
+        WidgetTester tester) async {
       final optionFinder = find.byType(CircularProgressIndicator);
       expect(optionFinder, findsOneWidget);
     }
 
-    testWidgets("HomePageScreen renders progress indicator while loading", (WidgetTester tester) async {
+    testWidgets("HomePageScreen renders progress indicator while loading",
+        (WidgetTester tester) async {
       await _setUpMoreScreen(tester);
       await _screenRendersProgressIndicatorWhileLoading(tester);
+    });
+  });
+
+  group('StatisticsAnalyzer', () {
+    test(
+        'Statistics functions return null when supplied Arrays are not same length',
+        () {
+      var testArrayA = randomArray(5);
+      var testArrayB = randomArray(7);
+
+      final StatisticsAnalyzer analyzer = StatisticsAnalyzer();
+
+      var resultCov = analyzer.calculateCovariance(testArrayA, testArrayB);
+      var resultPearson = analyzer.pearsonCorrelation(testArrayA, testArrayB);
+      var resultSpearman = analyzer.spearmanCorrelation(testArrayA, testArrayB);
+
+      expect(resultCov, null);
+      expect(resultPearson, null);
+      expect(resultSpearman, null);
+    });
+
+    test(
+        'Statistics functions return values when supplied Arrays are same length',
+        () {
+      var testArrayA = randomArray(5);
+      var testArrayB = randomArray(5);
+
+      final StatisticsAnalyzer analyzer = StatisticsAnalyzer();
+
+      var resultCov = analyzer.calculateCovariance(testArrayA, testArrayB);
+      var resultPearson = analyzer.pearsonCorrelation(testArrayA, testArrayB);
+      var resultSpearman = analyzer.spearmanCorrelation(testArrayA, testArrayB);
+
+      expect(resultCov.runtimeType, double);
+      expect(resultPearson.runtimeType, double);
+      expect(resultSpearman.runtimeType, double);
     });
   });
 }
